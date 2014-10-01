7d0ec9398f2242ecca03cb8ab3652a77b0d4c482
timaeudg <timaeudg@rose-hulman.edu>
2014-10-01 13:40:09 -0400

    Added command-line options for initializing a repository, and basic boilerplate for parsing, just need to add parsing code and file creation to have repo. Also updated the Dockerfile to include unit.js for unit testing

---
+++ cli/repoInit.js
@@ -0,0 +1,54 @@
+/**
+ * Created by Devon Timaeus on 9/30/2014.
+ */
+fs = require('fs');
+path = require('path');
+
+
+function getExtension(filename) {
+    var ext = path.extname(filename||'').split('.');
+    return ext[ext.length - 1];
+}
+
+function getFileContents(file) {
+    if (!fs.existsSync(file)) {
+        throw new URIError('No file found at: ' + file);
+    }
+
+    // We will want to do mimetype detection at some point, but for now, this is fine
+    var extension = getExtension(file);
+    if (extension.toLowerCase() != 'html') {
+        throw new TypeError('Filetype not HTML');
+    }
+
+    try {
+        return fs.readFileSync(file, 'utf8');
+    } catch (err) {
+        throw new URIError('No file found at: ' + file);
+    }
+}
+
+function initializeRepository(file) {
+    try {
+        var fileContents = getFileContents(file);
+    } catch (err) {
+        if (err instanceof TypeError) {
+            console.error(err.message);
+        }
+        if (err instanceof URIError) {
+            console.error(err.message);
+        }
+    }
+}
+
+/*
+    This makes the functions visible in other js files when using require
+    The first part is the key, and will specify what the function should be
+    called outside of this file, the second part is a function pointer to the
+    function that is desired to be associated with that name.
+ */
+module.exports = {
+    initializeRepository: initializeRepository,
+    getFileContents : getFileContents,
+    getExtension : getExtension
+};
