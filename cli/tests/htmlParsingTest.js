b76660328514459161de8e5f89e860527ddc3306
Ian <Obe_ty@yahoo.com>
2014-10-13 03:57:11 -0400

    a couple more tests added with a test file

--- cli/tests/htmlParsingTest.js
+++ cli/tests/htmlParsingTest.js
@@ -18,3 +18,28 @@ describe('Check POR-ids and id attributes', function() {
         }, ReferenceError);
     });
 });
+
+describe('parseHTML over all on test files', function(){
+	
+	it('Parses correct tags from HTML document', function() {
+		var validHTML = fs.readFileSync('./cli/tests/resources/testHTMLBasicFormat.html', 'utf8');
+		var output = parser.parseHTML(validHTML, 'repoName');
+		
+		assert.equal(output.children[1].children[0].children[0].porID, "id1");
+		assert.equal(output.children[1].children[1].children[0].porID, "id2");
+		assert.equal(output.children[1].children[1].children[0].children[1].porID, "id3");
+	});
+	
+	it('Parses text nodes correctly from HTML document', function(){
+		
+		var validHTML = fs.readFileSync('./cli/tests/resources/testHTMLBasicFormat.html', 'utf8');
+		var output = parser.parseHTML(validHTML, 'repoName');
+		
+		assert.equal(output.children[1].children[0].children[0].children[0].value, "\n\t\t\tSuper basic formatted html\n\t\t");
+		assert.equal(output.children[1].children[1].children[0].children[0].value, "\n\t\t\tpreSpan\n\t\t\t");
+		assert.equal(output.children[1].children[1].children[0].children[1].children[0].value, "\n\t\t\t\tinSpan\n\t\t\t");
+		assert.equal(output.children[1].children[1].children[0].children[2].value, " \n\t\t\tpostSpan\n\t\t");
+// 		This last one is weird, I don't know why it generates an extra space here. there is no space in the file.
+	});
+	
+});
