39b093551dc168cffc1a3a65e4af0e73922a48d3
satchwsm <satchwsm@rose-hulman.com>
2014-10-23 13:01:29 -0400

    Added in-page alerts

--- mockups/workflow mockups/init.js
+++ mockups/workflow mockups/init.js
@@ -3,7 +3,14 @@ window.onload = function () {
 
 document.forms["fileUpload"].onsubmit = function(){
 	var repoName = document.getElementById("repoName").value;
-	alert("New document \'" + repoName + "\' created successfully.");
+	var alertDiv = document.getElementById("alertDiv");
+	alertDiv.innerHTML = "<button onclick=\"clearAlert()\" id=\"button-alert\" class=\"pure-button button-alert\">New document created.</button>";
+
 }
 
 };
+
+function clearAlert(){
+	var alertDiv = document.getElementById("alertDiv");
+	alertDiv.innerHTML = "";
+}
