89b6a8aba877fda15a17be6a515936724c921df9
satchwsm <satchwsm@rose-hulman.com>
2014-10-31 00:12:02 -0400

    Added test for getPORObjectFromRepo

--- cli/tests/htmlWriterTest.js
+++ cli/tests/htmlWriterTest.js
@@ -68,14 +68,53 @@ describe('Test converting JSON object into a string', function() {
 
 });
 
+describe('Test creation of por object from repo', function() {
+
+    it('Create por object from a complex tree', function() {
+        var porObject = {
+        porID: "test",
+        metadata: {'constructionOrder':["doctype","3e61894fe84fd31246460272"]},
+        children: [{value:'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">', porID:"test"},
+                   {porID:"3e61894fe84fd31246460272",
+                        metadata: { 'tag': 'html',
+                                    'attributes': [],
+                                    'constructionOrder': [ '18eda53718376a1c58837e6e', '32bb8c5780eb0cbd283bc7a0' ] }, 
+                        children:[ { porID: '18eda53718376a1c58837e6e',
+                                        metadata: { 'tag': 'head',
+                                                    'attributes': [ {'name':"lang",'value':"en"} ],
+                                                    'constructionOrder': [ '9e2928948e789fe743dd9761', '5376f5329b6e80a8d7934c62' ] },
+                                        children: [ { porID: '9e2928948e789fe743dd9761',
+                                                    metadata: { 'tag': 'meta', 'attributes': [ {'name':"charset",'value':"UTF-8"} ], 'constructionOrder': [] },
+                                                    children: [] },
+                                                  { porID: '5376f5329b6e80a8d7934c62',
+                                                    metadata: { 'tag': 'title', 'attributes': [], 'constructionOrder': [ '8f422c29094277568d01bde4' ] },
+                                                    children: [ { value: 'titletext', porID: '5376f5329b6e80a8d7934c62' } ] } ] },
+                                   { porID: '32bb8c5780eb0cbd283bc7a0',
+                                        metadata: { 'tag': 'body', 'attributes': [], 'constructionOrder': [ 'derp' ] },
+                                        children: [ { porID: 'derp', 
+                                                      metadata: { 'tag': 'h1',
+                                                      'attributes': [ {'name':"id",'value':"derp"},{'name':"class",'value':"herp"},{'name':"name",'value':"headerOne"} ],
+                                                      'constructionOrder': 
+                                                          [ '63572ec1ae7313a2e128e0fe',
+                                                            '5892268b7a3588982d7042eb',
+                                                            '058f9fbe9cc061d2b819c905' ] }, 
+                                                      children: [ { value: 'Header ', porID: 'derp' },
+                                                                  { porID: '5892268b7a3588982d7042eb',
+                                                                    metadata: { 'tag': 'span', 'attributes': [], 'constructionOrder': [] },
+                                                                    children: [] },
+                                                                  { value: ' afterSpan', porID: 'derp' } ] } ] } ] } ] };
+        assert.equal(JSON.stringify(htmlWriter.getPORObjectFromRepo('./generationTest/testRepo/test')), JSON.stringify(porObject));
+    })
+});
+
 describe('Test initialization of local file', function() {
-    it('Creat html from a complex tree', function() {
+    it('Create html from a complex tree', function() {
         htmlWriter.initializeFile('./generationTest/testRepo/test', './resources/htmlInitOutput.html');
         assert.doesNotThrow( function() {
             fileExists('./resources/htmlInitOutput.html');
         } , Error);
     })
-})
+});
 
 
 function fileExists(fileLocation) {
