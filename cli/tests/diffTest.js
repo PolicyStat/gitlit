056a79d74de259a867c7fa3dc97a22847cb5ed12
satchwsm <satchwsm@rose-hulman.edu>
2015-01-13 01:48:57 -0500

    Added specific test html files, edited js test to init and commit to test repos for specific tests

--- cli/tests/diffTest.js
+++ cli/tests/diffTest.js
@@ -3,6 +3,7 @@ var htmlWriter = require('../htmlWriter');
 var repoWriter = require("../htmlRepoWriter");
 var repoInit = require("../repoInit.js");
 var parser = require('../htmlParser');
+var differ = require('../diffParser.js');
 var assert = require('assert');
 var path = require("path");
 var fs = require("fs");
@@ -10,25 +11,17 @@ var fs = require("fs");
 // preemptive guess on what the results of these approximate tests will be.
 
 describe('Diff result testing:', function () {
-	it ('handles the edit of text in a node', function(){
-		var currentPath = __dirname;
-		var firstFileName = "case1Before";
-		var secondFileName = "case1After";
-	    var pathToFirstFile = path.join(currentPath, 'resources', 'diffTesting', firstFileName);
-	    var pathToSecondFile = path.join(currentPath, 'resources', 'diffTesting', secondFileName);
-
-	    // they'll probably have to be turned into their intermediaries for comparison, right now this is fauxcode.
-	    var result = compare(pathToFirstFile, pathToSecondFile);
-	    var expected = [{isself:false, por_id:"40E36DB5C0AD13957351", child:0, modification:"add, delete", text:"First Text that has been altered"}];
-	    assert.equal(expected, result);
-	});
 
 	it ('handles the delete of text in a node', function(){
 		var currentPath = __dirname;
-		var firstFileName = "case2Before";
-		var secondFileName = "case2After";
+		var firstFileName = "deletionBefore";
+		var secondFileName = "deletionAfter";
 	    var pathToFirstFile = path.join(currentPath, 'resources', 'diffTesting', firstFileName);
 	    var pathToSecondFile = path.join(currentPath, 'resources', 'diffTesting', secondFileName);
+
+	    initAndCommit('deletionTest', pathToFirstFile, pathToSecondFile);
+	    var repoLocation = path.join(currentPath, 'deletionTest');
+	    var diff = differ.getDiff(repoLocation);
 
 	    var result = compare(pathToFirstFile, pathToSecondFile);
 	    var expected = [{isself:false, por_id:"40E36DB5C0AD13957351", child:0, modification:"delete"}];
@@ -37,10 +30,14 @@ describe('Diff result testing:', function () {
 
 	it ('handles the insertion of text in a node', function(){
 		var currentPath = __dirname;
-		var firstFileName = "case3Before";
-		var secondFileName = "case3After";
+		var firstFileName = "insertionBefore";
+		var secondFileName = "insertionAfter";
 	    var pathToFirstFile = path.join(currentPath, 'resources', 'diffTesting', firstFileName);
 	    var pathToSecondFile = path.join(currentPath, 'resources', 'diffTesting', secondFileName);
+
+	    initAndCommit('insertionTest', pathToFirstFile, pathToSecondFile);
+	    var repoLocation = path.join(currentPath, 'insertionTest');
+	    var diff = differ.getDiff(repoLocation);
 
 	    var result = compare(pathToFirstFile, pathToSecondFile);
 	    var expected = [{isself:false, por_id:"40E36DB5C0AD13957351", child:0, modification:"add", text:"First Text"}];
@@ -49,26 +46,25 @@ describe('Diff result testing:', function () {
 
 	it ('handles the move of text in a node', function(){
 		var currentPath = __dirname;
-		var firstFileName = "case1Before";
-		var secondFileName = "case1After";
+		var firstFileName = "moveBefore";
+		var secondFileName = "moveAfter";
 	    var pathToFirstFile = path.join(currentPath, 'resources', 'diffTesting', firstFileName);
 	    var pathToSecondFile = path.join(currentPath, 'resources', 'diffTesting', secondFileName);
+
+	    initAndCommit('moveTest', pathToFirstFile, pathToSecondFile);
+	    var repoLocation = path.join(currentPath, 'moveTest');
+	    var diff = differ.getDiff(repoLocation);
 
 	    var result = compare(pathToFirstFile, pathToSecondFile);
 	    var expected = [{isself:false, por_id:"40E36DB5C0AD13957351", child:0, modification:"delete"}, {isself:false, por_id:"40E36DB5C0AD13957351", child:1, modification:"add", text:"First Text"}];
 	    assert.equal(expected, result);
 	});
 
-	it ('handles the move and edit of text in a node', function(){
-		var currentPath = __dirname;
-		var firstFileName = "case1Before";
-		var secondFileName = "case1After";
-	    var pathToFirstFile = path.join(currentPath, 'resources', 'diffTesting', firstFileName);
-	    var pathToSecondFile = path.join(currentPath, 'resources', 'diffTesting', secondFileName);
-
-	    var result = compare(pathToFirstFile, pathToSecondFile);
-	    var expected = [{isself:false, por_id:"40E36DB5C0AD13957351", child:0, modification:"delete"}, {isself:false, por_id:"40E36DB5C0AD13957351", child:1, modification:"add", text:"This is my new First Text"}];
-	    assert.equal(expected, result);
-	});
 });
 
+function initAndCommit(repoName, file1, file2){
+	outputPath = __dirname;
+	repoInit.initializeRepository(file1, outputPath, repoName);
+	commitMessage = 'repoName test commit';
+	repoInit.commitDocument(file2, outputPath, repoName, commitMessage);
+}
