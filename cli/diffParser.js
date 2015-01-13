d7f46440c6cf29ccb23175f00efea8e968457b4d
timaeudg <timaeudg@rose-hulman.edu>
2015-01-13 02:20:01 -0500

    Have the ability to find the header info for the diff as well as the old and new versions of the file, this should enable us to do our diffing in the future.

--- cli/diffParser.js
+++ cli/diffParser.js
@@ -7,14 +7,17 @@ var deasync = require("deasync");
 var shellTools = require('./shellTools');
 
 function getDiff(repoLocation) {
-    // Note: This doesn't work currently, not sure why, but it's here for posterity for now
     var startingLocation = process.cwd();
     var resolvedRepoLocation = path.resolve(repoLocation);
     process.chdir(resolvedRepoLocation);
 
-    var command = 'git --no-pager diff HEAD^';
+    //The --unified=100000 guaranteed that we are given 100K lines of context.
+    //While this sounds insane, it guarantees that for each file's diff, we
+    //get the whole file, which will be nice for us when doing the diffing.
+    //Furthermore, it seems safe to assume that no one node will have more than
+    //100K lines of text, or at least, it shouldn't...
+    var command = 'git --no-pager diff --unified=100000 HEAD^';
     var diffOutput = shellTools.shellOut(command);
-//    console.log("output of the command is: " + diffOutput);
     process.chdir(startingLocation);
     return diffOutput;
 }
@@ -22,32 +25,75 @@ function getDiff(repoLocation) {
 function processDiffIntoFileGranules(diff) {
     var pattern = /(diff --git [^\s]* [^\s]*)/g;
     var match;
-    var matchIndeces = [];
+    var matchIndices = [];
     var counter = 0;
     while ((match = pattern.exec(diff)) !== null)
     {
-        matchIndeces.push(match.index);
+        matchIndices.push(match.index);
         counter++;
     }
-    matchIndeces.push(diff.length);
+    matchIndices.push(diff.length);
 
     var diffGranules = [];
-    for(var item = 0; item < (matchIndeces.length-1); item++) {
-        diffGranules.push(diff.substring(matchIndeces[item], matchIndeces[item+1]));
+    for(var item = 0; item < (matchIndices.length-1); item++) {
+        diffGranules.push(diff.substring(matchIndices[item], matchIndices[item+1]));
     }
     return diffGranules;
 }
 
 function convertFileGranulesIntoDiffObjects(granules) {
+    //TODO: make this operate on all the granules
     var granule = granules[0];
-    var splitGranule = granule.split("\n");
-    console.log(splitGranule);
-    var diffHeader = splitGranule[0].split(" ");
-    var oldFilePath = diffHeader[2].substring(2,diffHeader[2].length);
-    var newFilePath = diffHeader[3].substring(2,diffHeader[3].length);
+    var splitGranuleObject = splitGranuleIntoHeaderAndBodyObject(granule);
 
-    console.log("Old: " + oldFilePath);
-    console.log("New: " + newFilePath);
+    headerInterpretation(splitGranuleObject.header);
+
+    var fileVersions = getFileVersionsFromBody(splitGranuleObject.body);
+    console.log(fileVersions.newFile);
+
+}
+
+function headerInterpretation(header){
+    var diffHeaderList = header.split("\n");
+    var itemizedHeader = diffHeaderList[0].split(" ");
+//    console.log(diffHeaderList);
+    var oldFilePath = diffHeaderList[0].split(" ")[2].substring(2,itemizedHeader[2].length);
+    var newFilePath = diffHeaderList[0].split(" ")[3].substring(2,itemizedHeader[3].length);
+}
+
+function getFileVersionsFromBody(body) {
+    var bodyLines = body.split("\n");
+
+    var newFileTemplate = {idSymbol: '+'};
+    var oldFileTemplate = {idSymbol: '-'};
+
+    var newFileLines = getFileVersion(bodyLines, newFileTemplate);
+    var oldFileLines = getFileVersion(bodyLines, oldFileTemplate);
+    return {oldFile: oldFileLines, newFile: newFileLines};
+}
+
+function getFileVersion(fileArray, template){
+    var fileLines = [];
+
+    for(var line = 0; line < fileArray.length; line++) {
+        var lineToAdd = fileArray[line];
+        switch(fileArray[line][0]) {
+            case ' ':
+                fileLines.push(lineToAdd);
+                break;
+            case template.idSymbol:
+                fileLines.push(lineToAdd);
+                break;
+        }
+    }
+    return fileLines;
+}
+
+function splitGranuleIntoHeaderAndBodyObject(granule) {
+    var firstIndex = granule.indexOf("@@") + 2;
+    // The +2 is because we are looking for 2 characters
+    var secondIndex = firstIndex + (granule.substring(firstIndex, granule.length).indexOf("@@") + 2);
+    return {header: granule.substring(0, secondIndex), body: granule.substring(secondIndex, granule.length)};
 }
 
 module.exports = {
